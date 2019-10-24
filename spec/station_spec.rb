9127692dfd53b1e365a02fd2672d1e0382593579
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-10-24 14:50:52 +0100

    added a test & corresponding implementation

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,10 +1,14 @@
 require 'station'
 
 describe Station do
-  subject(:station) { described_class.new(name="Aldgate")}
+  subject(:station) { described_class.new(name="Aldgate", zone= 1)}
 
   it "has a name" do
     expect(station.name).to eq "Aldgate"
+  end
+
+  it "has a zone" do
+    expect(station.zone).to eq 1
   end
 
 end
